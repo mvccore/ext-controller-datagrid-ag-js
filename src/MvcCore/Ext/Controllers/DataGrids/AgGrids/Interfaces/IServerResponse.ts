4d8722acde64ae0d458400d0703cf25a02af944a
Tom Flídr <tomflidr@gmail.com>
2022-08-19 09:32:00 +0200

    Created interface for ajax server data response and initial data in html template.

---
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Interfaces/IServerResponse.ts
@@ -0,0 +1,11 @@
+declare namespace MvcCore.Ext.Controllers.DataGrids.AgGrids.Interfaces {
+	interface IServerResponse {
+		totalCount: number;
+		offset: number;
+		limit: number;
+		dataCount: number;
+		sorting: [string, 0 | 1][];
+		filtering: Map<string, Map<Enums.Operator, string[]>>;
+		data: any[];
+	}
+}
