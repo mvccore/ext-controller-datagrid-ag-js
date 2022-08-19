a22331396cc8c4909da1c548b3e7fda22b6e647a
Tom Flídr <tomflidr@gmail.com>
2022-08-19 09:32:48 +0200

    Controls texts enumeration.

---
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Enums/ControlText.ts
@@ -0,0 +1,12 @@
+namespace MvcCore.Ext.Controllers.DataGrids.AgGrids.Enums {
+	export enum ControlText {
+		PREVIOUS	= 'previous',
+		NEXT        = 'next',
+		FIRST       = 'first',
+		LAST        = 'last',
+		ALL         = 'all',
+		FILTER      = 'filter',
+		CLEAR       = 'clear',
+		STATUS      = 'status',
+	}
+}
