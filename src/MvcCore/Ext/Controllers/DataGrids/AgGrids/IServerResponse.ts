beb8ab2727f04e4338a2cb054c3c4445d874fc9e
Tom Flídr <tomflidr@gmail.com>
2022-08-19 09:35:44 +0200

    Removed old useless files.

--- src/MvcCore/Ext/Controllers/DataGrids/AgGrids/IServerResponse.ts
+++
@@ -1,9 +0,0 @@
-declare namespace MvcCore.Ext.Controllers.DataGrids.AgGrids {
-	interface IServerResponse {
-		TotalCount: number;
-		RowCount: number;
-		Data: any[];
-		StartRow: number;
-		EndRow: number;
-	}
-}
--- src/MvcCore/Ext/Controllers/DataGrids/AgGrids/ToolTips/IParams.ts
+++
@@ -1,9 +0,0 @@
-declare namespace MvcCore.Ext.Controllers.DataGrids.AgGrids.IToolTip {
-	interface IParams {
-		color?: string;
-		rowIndex: number;
-		api: {
-			getDisplayedRowAtIndex (index: number): agGrid.RowNode | undefined;
-		}
-	}
-}
