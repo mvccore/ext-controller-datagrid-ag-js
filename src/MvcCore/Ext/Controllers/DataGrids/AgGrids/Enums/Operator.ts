b9bc1c3b2046f570c4ba6fc92f4b9b59e7c3a0f1
Tom Flídr <tomflidr@gmail.com>
2022-08-19 09:32:28 +0200

    Filter operators enumeration.

---
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Enums/Operator.ts
@@ -0,0 +1,12 @@
+namespace MvcCore.Ext.Controllers.DataGrids.AgGrids.Enums {
+	export enum Operator {
+		NOT_EQUAL		= "!=",
+		LOWER			= "<",
+		LOWER_EQUAL		= "<=",
+		EQUAL			= "=",
+		GREATER			= ">",
+		GREATER_EQUAL	= ">=",
+		LIKE			= "LIKE",
+		NOT_LIKE		= "NOT LIKE",
+	}
+}
