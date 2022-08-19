ee1aefa0d5d00ff5773578f286de182a84dccfff
Tom Flídr <tomflidr@gmail.com>
2022-08-19 09:34:22 +0200

    Ajax data methods enumeration to specify how to request data in single page ajax data loading.

---
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Enums/AjaxDataRequestMethod.ts
@@ -0,0 +1,7 @@
+namespace MvcCore.Ext.Controllers.DataGrids.AgGrids.Enums {
+	export enum AjaxDataRequestMethod {
+		AJAX_DATA_REQUEST_METHOD_GET	= 1,
+		AJAX_DATA_REQUEST_METHOD_POST	= 2,
+		AJAX_DATA_REQUEST_METHOD_JSONP	= 4,
+	}
+}
