7794aa13486fa41cae360292057defb3b4a2b426
Tom Flídr <tomflidr@gmail.com>
2022-08-19 09:37:52 +0200

    Added helpers class, sorting, filtering and total count moved to local props, added getters for those local props.

--- src/MvcCore/Ext/Controllers/DataGrids/AgGrid.ts
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrid.ts
@@ -1,7 +1,7 @@
 namespace MvcCore.Ext.Controllers.DataGrids {
 	export class AgGrid {
-		protected serverConfig: AgGrids.IServerConfig;
-		protected initialData: AgGrids.IServerResponse;
+		protected serverConfig: AgGrids.Interfaces.IServerConfig;
+		protected initialData: AgGrids.Interfaces.IServerResponse;
 		protected events: AgGrids.Events;
 		protected initialization: AgGrids.Initializations;
 		protected grid: agGrid.Grid;
@@ -9,17 +9,25 @@ namespace MvcCore.Ext.Controllers.DataGrids {
 		protected gridOptions: agGrid.GridOptions<any>;
 		protected gridColumns: AgGrids.Types.GridColumn[];
 		protected gridDataSource: agGrid.IDatasource;
-		public constructor (serverConfig: AgGrids.IServerConfig, initialData: AgGrids.IServerResponse) {
-			this.serverConfig = serverConfig;
-			this.initialData = initialData;
+		protected helpers: AgGrids.Helpers;
+		protected sorting: [string, 0 | 1][];
+		protected filtering: Map<string, Map<AgGrids.Enums.Operator, string[]>>;
+		protected totalCount: number | null = null;
+		public constructor (serverConfig: AgGrids.Interfaces.IServerConfig, initialData: AgGrids.Interfaces.IServerResponse) {
+			this.helpers = new AgGrids.Helpers(this);
+			this.serverConfig = this.helpers.RetypeServerConfigObjects2Maps(serverConfig);
+			this.initialData = this.helpers.RetypeServerResponseObjects2Maps(initialData);
 			this.events = new AgGrids.Events(this);
-			this.initialization = new AgGrids.Initializations(this, this.events);
+			this.initialization = new AgGrids.Initializations(this, this.events, this.helpers);
+			this.sorting = this.initialData.sorting;
+			this.filtering = this.initialData.filtering;
+			this.totalCount = this.initialData.totalCount;
 		}
 
-		public GetServerConfig (): AgGrids.IServerConfig {
+		public GetServerConfig (): AgGrids.Interfaces.IServerConfig {
 			return this.serverConfig;
 		}
-		public GetInitialData (): AgGrids.IServerResponse {
+		public GetInitialData (): AgGrids.Interfaces.IServerResponse {
 			return this.initialData;
 		}
 		
@@ -61,6 +69,30 @@ namespace MvcCore.Ext.Controllers.DataGrids {
 		}
 		public GetGridDataSource (): agGrid.IDatasource {
 			return this.gridDataSource;
+		}
+
+		public SetSorting (sorting: [string, 0 | 1][]): this {
+			this.sorting = sorting;
+			return this;
+		}
+		public GetSorting (): [string, 0 | 1][] {
+			return this.sorting;
+		}
+
+		public SetFiltering (filtering: Map<string, Map<AgGrids.Enums.Operator, string[]>>): this {
+			this.filtering = filtering;
+			return this;
+		}
+		public GetFiltering (): Map<string, Map<AgGrids.Enums.Operator, string[]>> {
+			return this.filtering;
+		}
+
+		public SetTotalCount (totalCount: number | null): this {
+			this.totalCount = totalCount;
+			return this;
+		}
+		public GetTotalCount (): number | null {
+			return this.totalCount;
 		}
 	}
 }
